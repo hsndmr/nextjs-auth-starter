a6df0513df74eb4b47efb9d9e0fdd631a8b348f7
Hasan Demir <hsndmr5044@hotmail.com>
2023-03-18 02:09:18 +0300

    feat: add providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,32 @@
-import type { AppProps } from 'next/app';
 import '@/styles/globals.css';
+import { Layouts } from '@/layouts/Layouts';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '@/design/theme';
+import CssBaseline from '@mui/material/CssBaseline';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Head from 'next/head';
+import { MyAppProps } from '@/types/myAppTypes';
+import { GlobalStateProvider } from '@/providers/GlobalStateProvider';
+export default function App({ Component, pageProps }: MyAppProps) {
+  const Layout = Layouts[Component.Layout] ?? Layouts.Default;
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <GlobalStateProvider>
+          <CssBaseline />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </GlobalStateProvider>
+      </ThemeProvider>
+    </>
+  );
 }
